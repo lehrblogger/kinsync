beb855d949e14b8967571e6760bd6bcd4e81d0d6
Steven Lehrburger <lehrburger@gmail.com>
2024-06-05 21:53:13 -0700

    Add basic events to calendar for the blocks
    They still need more metadata and timezones though.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, Response
 from config import Config
 from ics import Calendar, Event
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 
 app = Flask(__name__)
@@ -37,9 +37,26 @@ def create_ics(trips):
                 if heading and date:
                     e = Event()
                     e.name = heading
-                    e.begin = datetime.strptime(date, "%Y-%m-%d")
+                    e.begin = datetime.strptime(date, '%Y-%m-%d')
                     e.make_all_day()
+                    e.url = f"https://wanderlog.com/plan/{trip.get('tripPlan', {}).get('key')}/"
                     calendar.events.add(e)
+                for block in section.get('blocks', []):
+                     placeName = block.get('place', {}).get('name')
+                     startTime = block.get('startTime')
+                     texts = block.get('text', {}).get('ops', [])
+                     title = texts[0].get('insert', '').split('\n')[0].strip('.') if texts else placeName
+                     if block.get('type') == 'place' and placeName and startTime:
+                        e = Event()
+                        e.name = title
+                        e.location = placeName
+                        e.begin = datetime.strptime(f'{date} {startTime}', '%Y-%m-%d %H:%M')
+                        endTime = block.get('endTime')
+                        if endTime:
+                            e.end =  datetime.strptime(f'{date} {endTime}', '%Y-%m-%d %H:%M')
+                        else:
+                            e.end = e.begin + timedelta(minutes=1)
+                        calendar.events.add(e)
     return calendar
 
 @app.route('/trips.json')
@@ -51,6 +68,16 @@ def trips_json():
 def sections_json():
     trips = [fetch_trip(trip_id) for trip_id in fetch_trip_ids()]
     return jsonify([section for trip in trips for section in trip.get('tripPlan', {}).get('itinerary', {}).get('sections', [])])
+
+@app.route('/blocks.json')
+def blocks_json():
+    trips = [fetch_trip(trip_id) for trip_id in fetch_trip_ids()]
+    sections = [section for trip in trips for section in trip.get('tripPlan', {}).get('itinerary', {}).get('sections', [])]
+    return [block 
+        for section in sections 
+        if section.get('mode') == 'dayPlan' and section.get('blocks')
+        for block in section.get('blocks') if block.get('type') == 'place'
+    ]
 
 @app.route('/trips.ics')
 def trips_ics():
